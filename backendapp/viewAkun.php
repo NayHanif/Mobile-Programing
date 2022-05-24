9c3e2650556c3c604f2edacecd2653264bfef55c
Nay Hanif <71371234+NayHanif@users.noreply.github.com>
2022-05-24 15:30:40 +0700

    Add files via upload

---
+++ backendapp/viewAkun.php
@@ -0,0 +1,33 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "minumin";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT id, firstname, lastname, berat, tinggi, kebutuhan FROM akun";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row[] = $result->fetch_assoc()) {
+    //echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    $item = $row;
+    $json = json_encode($item);
+  }
+} else {
+  echo "0 results";
+}
+echo $json;
+$conn->close();
+//reactnative/docs/image
+//docs/environment-setup
+//reactjs.org
+?>
